7cd5a6a504589e22514e1f8e360dd360a0e71dec
hong489 <yuhongyoong@gmail.com>
2023-10-04 09:20:16 +0800

    updated_template

---
+++ cb/ServicesTemplates/Factory.template.js
@@ -0,0 +1,8 @@
+module.export=`
+
+public function definition() 
+{
+    return [
+        '~cd-service-name~'=> $this->faker->'~cd-fakerType~'('~cd-fakerCondition1~','~cb-fakerCondition2~'),
+    ];
+}`;
